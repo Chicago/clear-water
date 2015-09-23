8add3b948cafe552922b899c9644380c74346d4c
Forest Gregg <fgregg@uchicago.edu>
2015-09-22 19:10:30 -0500

    weekdays analyses

--- analysis.R
+++ analysis.R
@@ -1,3 +1,15 @@
+prCurve <- function(truth, predicted_values) {
+    recalls = c()
+    precisions = c()
+    for (i in 0:20) {
+        recalls[i+1] = recall(truth, predicted_values >= i * 100)
+        precisions[i+1] = precision(truth, predicted_values >= i * 100)
+    }
+    
+    lines(recalls ~ precisions)
+}
+
+
 recall <- function(truth, predict) {
     return(sum(predict[truth])/sum(truth))
 }
@@ -21,24 +33,20 @@ measures <- measures[,c(1,2,3,4,5,8,9)]
 names(measures) <- c("beach", "date", "reading", "prediction", "status",
                      "yesterday_reading", "yesterday_prediction")
 
-recall(measures$reading > 1000, measures$yesterday_reading > 500)
-precision(measures$reading > 1000, measures$yesterday_reading > 500)
+true_ban_days <- measures$reading > 1000
 
-recall(measures$reading > 1000, measures$prediction > 200)
-precision(measures$reading > 1000, measures$prediction > 200)
+plot(c(0,1), c(0,1), type="n")
+#prCurve(true_ban_days,  measures$yesterday_reading)
+prCurve(true_ban_days,  measures$prediction)
 
-model.naive <- glm(reading ~ yesterday_reading*beach, measures, family='poisson')
+model.naive <- glm(reading ~ yesterday_reading*beach + weekdays(date)*beach, measures, family='poisson')
 summary(model.naive)
 
-recall(measures$reading > 1000, exp(predict(model.naive)) > 300)
-precision(measures$reading > 1000, exp(predict(model.naive)) > 300)
+#prCurve(true_ban_days,  exp(predict(model.naive)))
 
-
-
-model.forecast <- glm(reading ~ prediction*beach, measures, family='poisson')
+model.forecast <- glm(reading ~ prediction*beach + weekdays(date)*beach + months(date), measures, family='poisson')
 summary(model.forecast)
 
-recall(measures$reading > 1000, exp(predict(model.forecast)) > 700)
-precision(measures$reading > 1000, exp(predict(model.forecast)) > 700)
+prCurve(true_ban_days,  exp(predict(model.forecast)))
 
 
