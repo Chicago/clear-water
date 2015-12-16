2f9b41f9eaa44e9fa7e4b1947e438c752e19e35f
Tom Schenk Jr <tomschenkjr@gmail.com>
2015-12-15 19:10:50 -0600

    Draft of new code reorganizaton

--- analysis.R
+++ analysis.R
@@ -1,3 +1,52 @@
+source("data/ChicagoParkDistrict/raw/Standard 18 hr Testing/split_sheets.R")
+
+# Read-in data
+df2006 <- split_sheets("data/ChicagoParkDistrict/raw/Standard 18 hr Testing/2006 Lab Results.xls", 2006)
+df2007 <- split_sheets("data/ChicagoParkDistrict/raw/Standard 18 hr Testing/2006 Lab Results.xls", 2007)
+df2008 <- split_sheets("data/ChicagoParkDistrict/raw/Standard 18 hr Testing/2006 Lab Results.xls", 2008)
+df2009 <- split_sheets("data/ChicagoParkDistrict/raw/Standard 18 hr Testing/2006 Lab Results.xls", 2009)
+df2010 <- split_sheets("data/ChicagoParkDistrict/raw/Standard 18 hr Testing/2006 Lab Results.xls", 2010)
+df2011 <- split_sheets("data/ChicagoParkDistrict/raw/Standard 18 hr Testing/2006 Lab Results.xls", 2011)
+df2012 <- split_sheets("data/ChicagoParkDistrict/raw/Standard 18 hr Testing/2006 Lab Results.xls", 2012)
+df2013 <- split_sheets("data/ChicagoParkDistrict/raw/Standard 18 hr Testing/2006 Lab Results.xls", 2013)
+df2014 <- split_sheets("data/ChicagoParkDistrict/raw/Standard 18 hr Testing/2006 Lab Results.xls", 2014)
+df2015 <- split_sheets("data/ChicagoParkDistrict/raw/Standard 18 hr Testing/2006 Lab Results.xls", 2015)
+
+# Combine Data
+beach_readings <- rbind(df2006, df2007, df2008, df2009, df2010, df2011, df2012, df2013, df2014, df2015)
+
+# Clean Data
+## Remove greater or less-than markings
+beach_readings$Reading.1 <- factor(gsub(">", "", beach_readings$Reading.1)) # Remove greater-than marks
+beach_readings$Reading.2 <- factor(gsub(">", "", beach_readings$Reading.2)) # Remove greater-than marks
+beach_readings$Escherichia.coli <- factor(gsub(">", "", beach_readings$Escherichia.coli)) # Remove greater-than marks
+beach_readings$Reading.1 <- factor(gsub("<", "", beach_readings$Reading.1)) # Remove less-than marks
+beach_readings$Reading.2 <- factor(gsub("<", "", beach_readings$Reading.2)) # Remove less-than marks
+beach_readings$Escherichia.coli <- factor(gsub("<", "", beach_readings$Escherichia.coli)) # Remove less-than marks
+
+beach_readings <- unite_(beach_readings, "Full_date", c("Date", "Year"), sep=" ", remove=F)
+beach_readings$Full_date <- as.Date(beach_readings$Full_date, format="%B %d %Y") #now dates are sortable
+beach_readings$Weekday <- weekdays(beach_readings$Full_date) #add day of week
+beach_readings$Month <- format(beach_readings$Full_date,"%B")
+beach_readings$Day <- format(beach_readings$Full_date, "%d")
+
+
+# The code below can be activated once all of the numeric data is cleaned-up
+### Change readings to numeric data
+# beach_readings$Reading.1 <- as.numeric(beach_readings$Reading.1)
+# beach_readings$Reading.2 <- as.numeric(beach_readings$Reading.2)
+# beach_readings$Escherichia.coli <- as.numeric(beach_readings$Escherichia.coli)
+
+# Create measure variables
+# beach_readings$e.coli.geomean <- round(apply(cbind(beach_readings$Reading.1,beach_readings$Reading.2), 1, geometric.mean, na.rm=T), 1) 
+
+# Create measure variables
+# beach_readings$e.coli.geomean <- round(apply(cbind(beach_readings$Reading.1,beach_readings$Reading.2), 1, geometric.mean, na.rm=T), 1) 
+
+#create 1/0 for advisory at or over 235
+# beach_readings$elevated_levels <- ifelse(beach_readings$e.coli.geomean >= 235, 1, 0)
+
+# Analytics functions
 prCurve <- function(truth, predicted_values) {
     recalls = c()
     precisions = c()
