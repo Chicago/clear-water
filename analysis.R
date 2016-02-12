698b602c8e35ab686cbfefebb75991753d5cc3e2
mesweeney4 <sweeney4@gmail.com>
2016-02-11 21:15:30 -0600

    Fixes so there are no single days where the same beach appears more than once

--- analysis.R
+++ analysis.R
@@ -33,6 +33,17 @@ beach_readings$Day <- format(beach_readings$Full_date, "%d")
 ##Remove problematic dates
 beach_readings <- beach_readings[-which(beach_readings$Full_date %in% c(as.Date("2006-07-06"), as.Date("2006-07-08"), as.Date("2006-07-09"))),]
 
+##Remove 6 duplicates (12 total observations) -- beaches that have more than one reading on a day
+beach_readings=beach_readings[-which(beach_readings$Full_date=="2006-07-19" & beach_readings$Client.ID=="Jarvis/ Fargo"),]
+beach_readings=beach_readings[-which(beach_readings$Full_date=="2006-08-24" & beach_readings$Client.ID=="Jarvis"),]
+beach_readings=beach_readings[-which(beach_readings$Full_date=="2006-07-19" & beach_readings$Client.ID=="Hollywood/Ostermann"),]
+beach_readings=beach_readings[-which(beach_readings$Full_date=="2007-06-08" & beach_readings$Client.ID=="Hollywood/Thorndale*"),]
+beach_readings=beach_readings[-which(beach_readings$Full_date=="2008-06-08" & beach_readings$Client.ID=="Hollywood/Thorndale*"),]
+beach_readings=beach_readings[-which(beach_readings$Full_date=="2006-08-08" & beach_readings$Client.ID=="Loyola"),]
+
+##Remove 66 observations with date specified as "PM" -- which creates more than one reading per beach on a day
+beach_readings=beach_readings[!grepl("PM",beach_readings$Date),]
+
 ##Normalize beach names using names found on cpdbeaches.com
 cleanbeachnames <- read.csv("data/ChicagoParkDistrict/raw/Standard 18 hr Testing/cleanbeachnames.csv", stringsAsFactors=FALSE)
 changenames <- setNames(cleanbeachnames$New, cleanbeachnames$Old) 
@@ -74,69 +85,8 @@ source("data/ExternalData/merge_weather_sensor_data.r")
 # Bring in holiday data (only summer holidays)
 source("data/ExternalData/merge_holiday_data.r")
 
-# Bring in lock opening data
-source("data/ExternalData/merge_lock_data.r")
 
-# Bring in forecast.io daily weather data
-forecast_daily <- read.csv("data/ExternalData/forecastio_daily_weather.csv", stringsAsFactors = FALSE, row.names=NULL, header = T)
-forecast_daily <- unique(forecast_daily)
-beach_readings <- merge(x=beach_readings, y=forecast_daily, by.x=c("Client.ID", "Full_date"), by.y=c("beach", "time"), all.x=T)
-
-
-
-# Build naive logit model 
-# -----------------------------------------------------------
-
-beach_readings <- beach_readings[!is.na(beach_readings$Client.ID),]
-beach_readings <- beach_readings[order(beach_readings$Client.ID, beach_readings$Full_date),]
-
-# create "high reading" and "low reading"
-beach_readings$High.Reading <- mapply(max, beach_readings$Reading.1, beach_readings$Reading.2)
-beach_readings$Low.Reading <- mapply(min, beach_readings$Reading.1, beach_readings$Reading.2)
-
-# create columns for previous readings
-library(useful)
-temp <- split(beach_readings, beach_readings$Client.ID)
-for (i in 1:length(temp)) {
-  temp[[i]] <- shift.column(temp[[i]], columns=c("High.Reading","Low.Reading","e_coli_geomean_actual_calculated"), newNames=c("Previous.High.Reading", "Previous.Low.Reading", "Previous.Geomean"), len=1L, up=FALSE)
-}
-beach_readings <- do.call("rbind", temp)
-
-# use only records without NAs in predictors or response
-beach_readings_mod <- beach_readings[!is.na(beach_readings$Previous.High.Reading) & !is.na(beach_readings$Previous.Low.Reading) & !is.na(beach_readings$Previous.Geomean)& !is.na(beach_readings$elevated_levels_actual_calculated),]
-
-# get train and test set
-set.seed(12345)
-smp_size <- floor(0.75 * nrow(beach_readings_mod))
-train_ind <- sample(seq_len(nrow(beach_readings_mod)), size = smp_size)
-train <- beach_readings_mod[train_ind, ]
-test <- beach_readings_mod[-train_ind, ]
-
-# fit naive logit model to training set
-#fit <- glm(elevated_levels_actual_calculated ~ Previous.High.Reading + Previous.Low.Reading, data=train, family=binomial())
-fit <- glm(elevated_levels_actual_calculated ~ Previous.Geomean, data=train, family=binomial())
-summary(fit)
-
-# evaluate model on test set
-pred.prob=predict(fit, newdata=test, type="response")
-pred.elevated <- rep(0,nrow(test))
-pred.elevated[pred.prob>.5]=1
-test$prediction <- pred.elevated
-confmatrix=table(pred.elevated,test$elevated_levels_actual_calculated)
-confmatrix
-Recall=confmatrix[2,2]/(confmatrix[2,2]+confmatrix[1,2])
-Recall # 3%
-Precision=confmatrix[2,2]/(confmatrix[2,2]+confmatrix[2,1])
-Precision # 37%
-Fscore=(2*Precision*Recall)/(Precision+Recall)
-Fscore # 0.05
-Misclassification=1-(sum(diag(confmatrix))/nrow(test))
-Misclassification # 15%
-
-# -----------------------------------------------------------
-
-
-# Calculate confusion matrix for 2015 (EPA model)
+# Calculate confusion matrix in 2015
 
 beach_readings_2015 <- beach_readings[beach_readings$Year==2015 & 
                                          !is.na(beach_readings$Reading.1) & 
