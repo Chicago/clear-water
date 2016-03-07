ca86dcc42895213aefe52be388593958093e3c2c
Kevin Rose <kevin+gh@maypark.com>
2016-03-07 00:54:36 -0600

    adding in comparison to EPA

--- python_src/gbm.py
+++ python_src/gbm.py
@@ -94,22 +94,39 @@ if __name__ == '__main__':
     predictors = df.drop(['Full_date', 'Escherichia.coli'], axis=1)
     clfs = gbm(timestamps, predictors, classes)
 
+    df2 = rd.read_data()
+    # need to investigate why it's _x, what merge caused that?
+    df2 = df2[['Drek_Prediction_x', 'Escherichia.coli']].dropna()
+
     plt.figure(1)
     ax = plt.gca()
+    viz.roc(df2['Drek_Prediction_x'], df2['Escherichia.coli'] > 235,
+            ax=ax, block_show=False)
     c = ax.get_children()
     for i in range(18):
         c[i].set_alpha(.5)
-    ax.legend([c[i] for i in range(0,18,2)],
-              ['06', '07', '08', '09'] + [str(i) for i in range(10,15)],
+    c[18].set_color([0,0,0])
+    c[18].set_ls('-.')
+    c[18].set_linewidth(3)
+    c[18].set_alpha(.8)
+    ax.legend([c[i] for i in range(0,20,2)],
+              ['06', '07', '08', '09'] + [str(i) for i in range(10,15)] + ['EPA Model'],
               loc=4)
+
 
     plt.figure(2)
     ax = plt.gca()
     c = ax.get_children()
+    viz.precision_recall(df2['Drek_Prediction_x'], df2['Escherichia.coli'] > 235,
+                         ax=ax, block_show=False)
     for i in range(18):
         c[i].set_alpha(.5)
-    ax.legend([c[i] for i in range(0,18,2)],
-              ['06', '07', '08', '09'] + [str(i) for i in range(10,15)],
+    c[18].set_color([0,0,0])
+    c[18].set_ls('-.')
+    c[18].set_linewidth(3)
+    c[18].set_alpha(.8)
+    ax.legend([c[i] for i in range(0,20,2)],
+              ['06', '07', '08', '09'] + [str(i) for i in range(10,15)] + ['EPA Model'],
               loc=1)
 
     plt.draw()
