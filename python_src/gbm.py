3c305800d484ad7d53eda77254848d0905de5c14
Kevin Rose <kevin+gh@maypark.com>
2016-03-21 00:54:23 -0500

    Modified how data is pre-processed
    Should be easier to iterate on including different things. Note that the
    handling of NaN filling for E. coli levels was removed due to the
    remaining open questions about how it should be done. Similar models based
    only on weather data seem to acheieve similar levels of performance
    regardless.

--- python_src/gbm.py
+++ python_src/gbm.py
@@ -3,12 +3,13 @@ import sklearn
 import sklearn.ensemble
 import read_data as rd
 import visualizations as viz
-import pandas as pd
 import matplotlib.pyplot as plt
 
 
 def gbm(timestamps, predictors, classes):
-
+    '''
+    TODO
+    '''
     timestamps = timestamps.map(lambda x: x.year)
 
     start = timestamps.min()
@@ -25,7 +26,7 @@ def gbm(timestamps, predictors, classes):
 
         clf = sklearn.ensemble.GradientBoostingClassifier(
             n_estimators=100, learning_rate=0.05,
-            max_depth=4, subsample=0.7, verbose=True
+            max_depth=4, subsample=0.9, verbose=True
         )
         clf.fit(predictors.ix[train_indices,:], classes[train_indices])
 
@@ -40,104 +41,141 @@ def gbm(timestamps, predictors, classes):
 
 
 def prepare_data(df=None):
+    '''
+    TODO
+    '''
     if df is None:
         df = rd.read_data()
 
+    # Leaving 2015 as the final validation set
     df = df[df['Full_date'] < '1-1-2015']
 
-    df = df[['Full_date', 'Client.ID', 'Weekday', 'Escherichia.coli',
-             'cloudCover', 'temperatureMin', 'temperatureMax',
-             'precipIntensityMax', 'precipProbability']]
+    ######################################################
+    #### Add derived columns here
+    ######################################################
 
-    df2 = rd.add_column_prior_data(
-        df, ['Escherichia.coli', 'cloudCover', 'temperatureMin', 'temperatureMax',
-             'precipIntensityMax', 'precipProbability'],
-        range(1,8), beach_col_name='Client.ID', timestamp_col_name='Full_date'
+    df['DayOfYear'] = df['Full_date'].map(lambda x: x.dayofyear)
+
+
+    ######################################################
+    #### List all columns you will use
+    ######################################################
+
+    # Meta columns are not used as predictors
+    meta_columns = ['Full_date', 'Escherichia.coli']
+
+    # Deterministic columns are known ahead of time, their actual values are used
+    # with no previous days being used. If you wish to have a determinstic value
+    # while also including historical values, then the current work-around is to
+    # put that column in both the determinstic and the historical lists.
+    deterministic_columns = ['Client.ID', 'Weekday', 'sunriseTime',
+                             'DayOfYear']
+
+    # Historical columns have their previous days' values added to the predictors,
+    # but not the current day's value(s). The value NUM_LOOKBACK_DAYS set below
+    # controls the number of previous days added. Nothing is currently done to
+    # fill NA values here, so if you wish to use columns with a high rate of data
+    # loss, then you should add logic to fill the NA values.
+    historical_columns = ['precipIntensity', 'precipIntensityMax',
+                          'temperatureMin', 'temperatureMax',
+                          'humidity', 'windSpeed', 'cloudCover']
+
+    # Each historical column will have the data from 1 day back, 2 days back,
+    # ..., NUM_LOOKBACK_DAYS days back added to the predictors.
+    NUM_LOOKBACK_DAYS = 3
+
+
+    ######################################################
+    #### Get relevant columns, add historical data
+    ######################################################
+
+    all_columns = meta_columns + deterministic_columns + historical_columns
+
+    df = df[all_columns]
+
+    df = rd.add_column_prior_data(
+        df, historical_columns, range(1, NUM_LOOKBACK_DAYS + 1),
+        beach_col_name='Client.ID', timestamp_col_name='Full_date'
     )
 
-    for col in ['Escherichia.coli', 'cloudCover', 'temperatureMin', 'temperatureMax', 'precipIntensityMax', 'precipProbability']:
-        all_prior_columns = df2.columns.map(lambda x: x[1:12+len(col)]) == '_day_prior_' + col
-        df2[col + '7_day_moving_avg'] = df2[df2.columns[all_prior_columns]].mean(axis=1)
+    df.drop(historical_columns, axis=1, inplace=True)
 
-        df = pd.merge(df, df2[['Full_date', 'Client.ID', col + '7_day_moving_avg', '1_day_prior_' + col]],
-                      how='left', on=['Full_date', 'Client.ID'])
 
-        df['Prior' + col] =  pd.Series(map((lambda x,y: y if np.isnan(x) else x),
-                                           df['1_day_prior_' + col], df[col + '7_day_moving_avg']))
-        df = df.drop([col + '7_day_moving_avg', '1_day_prior_' + col], axis=1)
-
-    for b in df['Client.ID'].unique().tolist():
-        beach2 = df.ix[df['Client.ID']==b,['Full_date','Client.ID','PriorEscherichia.coli']].reset_index()
-        c = beach2.columns.tolist()
-        c[c.index('PriorEscherichia.coli')] = b + '_PriorEscherichia.coli'
-        beach2.columns = c
-        beach2.drop(['index','Client.ID'], axis=1, inplace=True)
-        df = pd.merge(df, beach2, on='Full_date', how='left')
-
-    priorEColiColumns = [b + '_PriorEscherichia.coli' for b in df['Client.ID'].unique().tolist()]
-    df['city_mean'] = df[priorEColiColumns].mean(axis=1)
-    fill_value = pd.DataFrame({col: df['city_mean'] for col in priorEColiColumns})
-    df.fillna(fill_value, inplace=True)
+    ######################################################
+    #### Process non-numeric columns
+    ######################################################
 
     # process all of the nonnumeric columns
+    # This method just assigns a numeric value to each possible value
+    # of the non-numeric column. Note that this will not work well
+    # for regression-style models, where instead dummy columns should
+    # be created.
     def nonnumericCols(data, verbose=True):
-    	for f in data.columns:
-    		if data[f].dtype=='object':
-    			if (verbose):
-    				print(f)
-    			lbl = sklearn.preprocessing.LabelEncoder()
-    			lbl.fit(list(data[f].values))
-    			data[f] = lbl.transform(list(data[f].values))
-    	return data
+        for f in data.columns:
+            if data[f].dtype=='object':
+                if (verbose):
+                    print('Column ' + str(f) + ' being treated as non-numeric')
+                lbl = sklearn.preprocessing.LabelEncoder()
+                lbl.fit(list(data[f].values))
+                data[f] = lbl.transform(list(data[f].values))
+        return data
+
     df = nonnumericCols(df)
+
+
+    ######################################################
+    #### Drop any rows that still have NA, set up outputs
+    ######################################################
+
     df.dropna(axis=0, inplace=True)
 
-    return df
+    predictors = df.drop(['Escherichia.coli', 'Full_date'], axis=1)
+    meta_info = df[['Escherichia.coli', 'Full_date']]
+
+    return predictors, meta_info
 
 
 if __name__ == '__main__':
-    df = prepare_data()
-    timestamps = df['Full_date']
-    classes = df['Escherichia.coli'] > 235
-    predictors = df.drop(
-        ['Full_date', 'Escherichia.coli',  'cloudCover',
-         'temperatureMin', 'temperatureMax', 'precipIntensityMax', 'precipProbability'],
-        axis=1
-    )
+    predictors, meta_info = prepare_data()
+    timestamps = meta_info['Full_date']
+    classes = meta_info['Escherichia.coli'] > 235
+
+    print('Using the following columns as predictors:')
+    for c in predictors.columns:
+        print('\t' + str(c))
     clfs = gbm(timestamps, predictors, classes)
-    print(predictors.columns.tolist())
 
     df2 = rd.read_data()
     df2 = df2[['Drek_Prediction', 'Escherichia.coli']].dropna()
 
-    plt.figure(1)
-    ax = plt.gca()
+    N = 18
+
+    ax = plt.figure(1).get_axes()[0]
     viz.roc(df2['Drek_Prediction'], df2['Escherichia.coli'] > 235,
             ax=ax, block_show=False)
     c = ax.get_children()
-    for i in range(18):
+    for i in range(N):
         c[i].set_alpha(.5)
-    c[18].set_color([0,0,0])
-    c[18].set_ls('-.')
-    c[18].set_linewidth(3)
-    c[18].set_alpha(.8)
-    ax.legend([c[i] for i in range(0,20,2)],
+    c[N].set_color([0,0,0])
+    c[N].set_ls('--')
+    c[N].set_linewidth(3)
+    c[N].set_alpha(.8)
+    ax.legend([c[i] for i in range(0,N+2,2)],
               ['06', '07', '08', '09'] + [str(i) for i in range(10,15)] + ['EPA Model'],
               loc=4)
 
 
-    plt.figure(2)
-    ax = plt.gca()
-    c = ax.get_children()
+    ax = plt.figure(2).get_axes()[0]
     viz.precision_recall(df2['Drek_Prediction'], df2['Escherichia.coli'] > 235,
                          ax=ax, block_show=False)
-    for i in range(18):
+    c = ax.get_children()
+    for i in range(N):
         c[i].set_alpha(.5)
-    c[18].set_color([0,0,0])
-    c[18].set_ls('-.')
-    c[18].set_linewidth(3)
-    c[18].set_alpha(.8)
-    ax.legend([c[i] for i in range(0,20,2)],
+    c[N].set_color([0,0,0])
+    c[N].set_ls('--')
+    c[N].set_linewidth(3)
+    c[N].set_alpha(.8)
+    ax.legend([c[i] for i in range(0,N+2,2)],
               ['06', '07', '08', '09'] + [str(i) for i in range(10,15)] + ['EPA Model'],
               loc=1)
 
