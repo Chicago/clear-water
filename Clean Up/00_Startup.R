22f109fc1b2c0425635adb6dbe8eab4fd26248fe
Callin <callin620@gmail.com>
2016-11-02 14:28:59 -0500

    Better Sourcing

--- Clean Up/00_Startup.R
+++ Clean Up/00_Startup.R
@@ -1,6 +1,14 @@
 #This is to grab all of the sources and packages needed to run all these scripts.
-cwd<-getwd()
-file<-paste(cwd,"/Functions/usePackage.R",sep="")
-source(file)
+
+#A function to go in and grab all the .R scripts in a file
+sourceDir <- function(path, trace = TRUE, ...) {
+  for (nm in list.files(path, pattern = "\\.[Rr]$")) {
+    if(trace) cat(nm,":")           
+    source(file.path(path, nm), ...)
+    if(trace) cat("\n")
+  }
+}
+
+sourceDir(paste(getwd(),"/Functions",sep=""))
 usePackage("lubridate")
 usePackage("RSocrata")
