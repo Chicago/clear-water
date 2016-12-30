515aab79e61ce5f4b15355564ab49346469c07fe
Lucius <nicholas.lucius2@cityofchicago.org>
2016-12-30 10:58:16 -0600

    Deal with date/time problem (see #119)

--- Functions/addLabsColumn.R
+++ Functions/addLabsColumn.R
@@ -3,13 +3,20 @@
 addLabsColumn <- function(df, beach, column) {
   new_col <- c()
   for (row in c(1:nrow(df))) {
-    this_date <- df$Date[row]
-    new_value <- df[df$Client.ID == beach & df$Date == this_date,column]
+    this_date <- as.Date(df$Date[row])
+    new_value <- df[df$Client.ID == beach & as.Date(df$Date) == this_date,column]
     if (length(new_value) == 0)
       new_col <- c(new_col, NA)
+    # if there are multiple readings for a beach/day combo
+    # use geomean of readings
+    else if (length(new_value) > 1) {
+      new_value <- prod(as.numeric(new_value))^(1/length(new_value))
+      new_col <- c(new_col, new_value)
+    }
     else
       new_col <- c(new_col, new_value)
   }
+  print(paste0("Adding Column: ",beach,"_",column))
   df <- cbind(df, new_col)
   colnames(df)[ncol(df)] <- paste0(beach,"_",column)
   df
